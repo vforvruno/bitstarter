d3170da5b57cd70e246ed149a5782be3ef81cf21
Bruno Giacomelli <gvruno@gmail.com>
2013-07-10 05:31:12 +0000

    finished part1

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = fs.readFileSync('index.html');
+  var text = buf.toString('utf8', 0, buf.length);
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
